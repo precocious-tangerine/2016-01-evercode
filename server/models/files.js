340b4a11194799e95c54c8fc81974d84b1a770b2
Edison Huff <b@mboost.net>
2016-03-10 15:10:40 -0800

    Replaced all instances of "snippet" with "file".

---
+++ server/models/files.js
@@ -0,0 +1,27 @@
+'use strict';
+const Promise = require('bluebird');
+var db = require('../db/database');
+var _ = require('lodash');
+
+class Files = () => {
+};
+
+
+Files.prototype.newFilesAsync = (file) => {
+  
+}
+
+Files.prototype.getFilesAsync = (fileName) => {
+  
+}
+
+Files.prototype.updateFilesAsync = (file) => {
+ 
+}
+
+Files.prototype.getFilesAsync = (fileName) => {
+  
+}
+
+
+module.exports = Files;
