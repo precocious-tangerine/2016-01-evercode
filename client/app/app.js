237278f197a61991ef22f25a56fa94036d2d0e6d
Max Kroshka <max.kroshka@gmail.com>
2016-03-10 12:28:30 -0800

    Structure index and app files

--- client/app/app.js
+++ client/app/app.js
@@ -1,1 +1,26 @@
-i 
+angular.module('evercode', [])
+.config(function ($stateProvider, $urlRouterProvider){
+  $urlRouterProvider.otherwise('/login');
+
+  $stateProvider
+    .state('login', {
+      url: '/login',
+      templateUrl: 'app/auth/signin.html',
+      controller: ''
+    })
+    .state('login', {
+      url: '/login',
+      templateUrl: 'app/auth/signin.html',
+      controller: ''
+    })
+    .state('login', {
+      url: '/login',
+      templateUrl: 'app/auth/signin.html',
+      controller: ''
+    })
+    .state('login', {
+      url: '/login',
+      templateUrl: 'app/auth/signin.html',
+      controller: ''
+    })
+})
