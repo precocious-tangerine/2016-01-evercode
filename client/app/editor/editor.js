3f2988c9314a3678bdfac5d812f1ea869226fb06
Gerrit <gerrityntema@gmail.com>
2016-03-10 19:42:28 -0800

    Add editor. Add annotations files

--- client/app/editor/editor.js
+++ client/app/editor/editor.js
@@ -1,4 +1,10 @@
 angular.module('evercode.editor', [])
-.controller('EditorCtrl', function($scope) {
-
-});
+.controller('EditorCtrl', [ '$scope', function($scope) {
+    $scope.editorOptions = {
+        lineWrapping : true,
+        lineNumbers: true,
+        readOnly: 'nocursor',
+        mode: 'xml',
+        value: 'hello'
+    };
+}]);
