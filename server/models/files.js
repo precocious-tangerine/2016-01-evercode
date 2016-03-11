5945f2866d9bceaa64666409e57a76fdf29a2a2b
Edison Huff <b@mboost.net>
2016-03-10 20:35:54 -0800

    Created basic fileSchema and outline for helper functions

--- server/models/files.js
+++ server/models/files.js
@@ -1,27 +1,41 @@
 'use strict';
 const Promise = require('bluebird');
-var db = require('../db/database');
-var _ = require('lodash');
+let mongoose = require('mongoose');
+let _ = require('lodash');
 
-class Files {
-};
+let fileSchema = mongoose.Schema({
+	data: {type: String, required: true},
+	createdBy: { type: String, required: true },
+	filePath: { type: String },
+	labels: { type: Schema.Types.Mixed, default: {} }
+	shareUrl: { type: String },
+});
 
+let File = mongoose.model('File',fileSchema);
 
-Files.prototype.newFilesAsync = (file) => {
-  
+File.getFile(_id, callback){
+	return File.findOne({_id: new ObjectId(_id)}).exec()
+		.then((fileObj) => {
+		 	callback(fileObj);
+		})
+		.catch((err) => {
+		 	console.log("error", err);
+		});
 }
 
-Files.prototype.getFilesAsync = (fileName) => {
-  
+File.getFileByUser(user_Id, callback){
+	return File.find({createdBy: nuser_Id}).exec()
+		.then((foundFiles) => {
+			if(Array.isArray(foundFiles) && foundFiles.length !== 0){
+			 	callback(fileObj);
+			 	return
+			}
+			callback({message: 'password invalid'}, null);
+		})
+		.catch((err) => {
+		 	console.log("error", err);
+		});
 }
 
-Files.prototype.updateFilesAsync = (file) => {
- 
-}
+module.exports = File;
 
-Files.prototype.getFilesAsync = (fileName) => {
-  
-}
-
-
-module.exports = Files;
