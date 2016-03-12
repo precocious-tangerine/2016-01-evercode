f44c24b146c7985eba9969b9e87487da7d7b96f9
Orlando Castillo <orlandoc@sas.upenn.edu>
2016-03-11 19:59:41 -0800

    Fix syntax errors in reducers

--- client/app/redux/reducers.js
+++ client/app/redux/reducers.js
@@ -2,9 +2,9 @@ import {combineReducers} from 'redux';
 
 const modifySelectedSnippet = (state = '', action) => {
 	switch (action.type) {
-		case: 'SET_SELECTED_SNIPPET':
+		case 'SET_SELECTED_SNIPPET':
 			return action.snippet || 'modify the reducer for snippet objects';
-		case: 'REMOVE_SELECTED_SNIPPET':
+		case 'REMOVE_SELECTED_SNIPPET':
 			return '';
 		default:
 			return state;
@@ -13,9 +13,9 @@ const modifySelectedSnippet = (state = '', action) => {
 
 const modifySelectedFolder = (state = '', action) => {
 	switch (action.type) {
-		case: 'SET_SELECTED_FOLDER':
+		case 'SET_SELECTED_FOLDER':
 			return action.folder || 'modify the reducer for folder objects';
-		case: 'REMOVE_SELECTED_FOLDER':
+		case 'REMOVE_SELECTED_FOLDER':
 			return '';
 		default:
 			return state;
@@ -23,21 +23,22 @@ const modifySelectedFolder = (state = '', action) => {
 }
 
 const modifyFileTree = (state = {snippets: [], folders: []}, action) => {
+	let newSnippets, newFolders, newTree;
 	switch (action.type) {
-		case: 'ADD_NEW_SNIPPET'
-			let newSnippets = state.snippets.push('snippet1');
+		case 'ADD_NEW_SNIPPET':
+			newSnippets = state.snippets.push('snippet1');
 			return Object.assign({}, state, {snippet: newSnippets});
-		case: 'REMOVE_SNIPPET'
-			let newSnippets = state.snippets.pop();
+		case 'REMOVE_SNIPPET':
+			newSnippets = state.snippets.pop();
 			return Object.assign({}, state, {snippets: newSnippets});
-		case: 'ADD_NEW_FOLDER'
-			let newFolders = state.folders.push('folder1');
+		case 'ADD_NEW_FOLDER':
+			newFolders = state.folders.push('folder1');
 			return Object.assign({}, state, {folders: []});
-		case: 'REMOVE_NEW_FOLDER'
-			let newFolders = state.folders.pop();
+		case 'REMOVE_NEW_FOLDER':
+			newFolders = state.folders.pop();
 			return Object.assign({}, state, {folders: []});
-		case: 'SET_FILE_TREE'
-			let newTree = {
+		case 'SET_FILE_TREE':
+			newTree = {
 				snippets: ['newSnippet,newTree'],
 				folders: ['newFolder,newTree']
 			};
