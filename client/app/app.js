0804ce1dba854472cfb1fdb0764f3e4e58e8a6aa
Orlando Castillo <orlandoc@sas.upenn.edu>
2016-03-12 15:24:57 -0800

    Implement refactoring of AuthCtrl, and services, and signin route

--- client/app/app.js
+++ client/app/app.js
@@ -2,76 +2,73 @@
 import angular from 'angular';
 import ngRedux from 'ng-redux';
 import createLogger from 'redux-logger';
-import finalReducer from './redux/reducers.js';
+import {finalReducer} from './redux/reducers.js';
 import angular_ui_router from 'angular-ui-router';
 import angular_ui_codemirror from 'angular-ui-codemirror';
-
-
-angular.module('evercode', ['evercode.directories', 'evercode.services', 'evercode.auth', 'evercode.snippets', 'evercode.editor', angular_ui_router, angular_ui_codemirror, ngRedux])
+import {createAuthCtrl} from './auth/auth.js';
+import {Folders, Auth} from './services/services.js'
+angular.module('evercode', [angular_ui_router, ngRedux])
   .config(($stateProvider, $urlRouterProvider, $httpProvider, $ngReduxProvider) => {
-    $urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('/signin');
     $stateProvider
-      .state('login', {
-        url: '/signin',
-        templateUrl: './app/auth/signin.html',
-        controller: 'AuthCtrl',
-        access: {restricted: false}
-      })
-      .state('signup', {
-        url: '/signup',
-        templateUrl: 'app/auth/signup.html',
-        controller: 'AuthCtrl',
-        access: {restricted: false}
-      })
-      .state('signout', {
-        resolve: {
-          signout: function(Auth) {
-            return Auth.signout();   
-          }
-        },
-        access: {restricted: false}
-      })
-      .state('main', {
-        url: '/main',
-        templateUrl: './app/directories/main.html',
-        controller: 'DirectoriesCtrl',
-        access: {restricted: true}
-      })
-      .state('main.snippets', {
-        url: '/:folder',
-        templateUrl: 'app/snippets/snippets.html',
-        controller: 'SnippetsCtrl',
-        access: {restricted: true}
-      })
-      .state('main.snippets.editor', {
-         url: '/editor',
-         templateUrl: 'app/editor/editor.html',
-         controller: 'EditorCtrl',
-         access: {restricted: true}
-       })
+      .state('login', createAuthCtrl('/signin'))
+      // .state('signup', {
+      //   url: '/signup',
+      //   templateUrl: 'app/auth/signup.html',
+      //   controller: 'AuthCtrl',
+      //   access: {restricted: false}
+      // })
+      // .state('signout', {
+      //   resolve: {
+      //     signout: function(Auth) {
+      //       return Auth.signout();   
+      //     }
+      //   },
+      //   access: {restricted: false}
+      // })
+      // .state('main', {
+      //   url: '/main',
+      //   templateUrl: './app/directories/main.html',
+      //   controller: 'DirectoriesCtrl',
+      //   access: {restricted: true}
+      // })
+      // .state('main.snippets', {
+      //   url: '/:folder',
+      //   templateUrl: 'app/snippets/snippets.html',
+      //   controller: 'SnippetsCtrl',
+      //   access: {restricted: true}
+      // })
+      // .state('main.snippets.editor', {
+      //    url: '/editor',
+      //    templateUrl: 'app/editor/editor.html',
+      //    controller: 'EditorCtrl',
+      //    access: {restricted: true}
+      //  })
 
-    $httpProvider.interceptors.push('AttachTokens');
+    // $httpProvider.interceptors.push('AttachTokens');
 
     $ngReduxProvider.createStoreWith(finalReducer, [createLogger()]);
   })
-  .factory('AttachTokens', ($window) => {
-    var attach = {
-      request: (object) => {
-        var jwt = $window.localStorage.getItem('com.evercode');
-        if (jwt) {
-          object.headers['x-access-token'] = jwt;
-        }
-        object.headers['Allow-Control-Allow-Origin'] = '*';
-        return object;
-      }
-    };
-    return attach;
-  })
-  .run(($rootScope, $location, Auth) => {
-    $rootScope.$on('$stateChangeStart', (evt, next, current) => {
-      $rootScope.location = $location.path();
-      if (next.access.restricted && !Auth.isAuth()) {
-        $location.path('/signin');
-      }
-    });
-  });
+  .service('Auth', Auth)
+  .service('Folders', Folders);
+  // .factory('AttachTokens', ($window) => {
+  //   var attach = {
+  //     request: (object) => {
+  //       var jwt = $window.localStorage.getItem('com.evercode');
+  //       if (jwt) {
+  //         object.headers['x-access-token'] = jwt;
+  //       }
+  //       object.headers['Allow-Control-Allow-Origin'] = '*';
+  //       return object;
+  //     }
+  //   };
+  //   return attach;
+  // })
+  // .run(($rootScope, $location, Auth) => {
+  //   $rootScope.$on('$stateChangeStart', (evt, next, current) => {
+  //     $rootScope.location = $location.path();
+  //     if (next.access.restricted && !Auth.isAuth()) {
+  //       $location.path('/signin');
+  //     }
+  //   });
+  // });
--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,37 +1,50 @@
-angular.module('evercode.auth', [])
 
-.controller('AuthCtrl', function($scope, $location, Auth, $window) {
-  $scope.user = {};
-  $scope.failed = true;
+export const createAuthCtrl = (url) => {
+  return {
+    url: url,
+    controllerAs: 'authCtrl',
+    controller: AuthCtrl,
+    template: require('./signin.html'),
+    scope: {}
+  }
+}
 
-  $scope.signin = function(boolean) {
-    $scope.failed = true;
-    if (boolean) {
-      Auth.signin($scope.user)
-        .then(function(token) {
-          $window.localStorage.setItem('com.evercode', token);
-          $location.path('/main');
-        })
-        .catch(function(error) {
-          $scope.failed = false;
-          console.error(error);
-        });
-    }
-  };
 
-  $scope.signup = function(boolean) {
-    $scope.failed = true;
-    if (boolean) {
-      Auth.signup($scope.user)
-        .then(function(token) {
-          $window.localStorage.setItem('com.evercode', token);
-          $location.path('/main');
-        })
-        .catch(function(error) {
-          $scope.failed = false;
-          console.error(error);
-        });
-    }
-  };
+class AuthCtrl {
+  constructor($scope, $location, $window, Auth) {
+  this.user = {};
+  this.failed = true;
+  }
+  
+  signin(boolean) {
+    this.failed = true;
+    // if (boolean) {
+      // Auth.signin($scope.user)
+      //   .then(function(token) {
+      //     $window.localStorage.setItem('com.evercode', token);
+      //     $location.path('/main');
+      //   })
+      //   .catch(function(error) {
+      //     $scope.failed = false;
+      //     console.error(error);
+      //   });
+      //}
+  }
+  
 
-});
+  signup(boolean) {
+    this.failed = true;
+    // if (boolean) {
+    //   Auth.signup($scope.user)
+    //     .then(function(token) {
+    //       $window.localStorage.setItem('com.evercode', token);
+    //       $location.path('/main');
+    //     })
+    //     .catch(function(error) {
+    //       $scope.failed = false;
+    //       console.error(error);
+    //     });
+    // }
+  }
+
+}
