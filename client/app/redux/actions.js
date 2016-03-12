ccf49c17019838bf299b78dbc3fa71b977e0946d
Orlando Castillo <orlandoc@sas.upenn.edu>
2016-03-11 19:24:35 -0800

    Fill in actions file, initialize reducers file

---
+++ client/app/redux/actions.js
@@ -0,0 +1,64 @@
+
+//Handling which snippet is currently selected
+export const setSelectedSnippet = (snippet) => {
+	return {
+		type: 'SET_SELECTED_SNIPPET',
+		file
+	};
+};
+export const removeSelectedSnippet = () => {
+	return {
+		type: 'REMOVE_SELECTED_SNIPPET'
+	};
+};
+
+
+//Handling which folder is currently selected
+export const setSselectedFolder = (folder) => {
+	return {
+		type: 'SET_SELECTED_FOLDER',
+		folder
+	};
+};
+export const removeSelectedFolder = () => {
+	return {
+		type: 'REMOVE_SELECTED_SNIPPET'
+	};
+};
+
+
+//Handling adding/removing snippets to main file tree
+export const addSnippetToFileTree = (snippet) => {
+	return {
+		type: 'ADD_NEW_SNIPPET'
+		snippet
+	};
+};
+export const removeSnippetFileTree = (snippet) => {
+	return {
+		type: 'REMOVE_SNIPPET'
+		snippet
+	};
+};
+
+//Handling adding/removing folder to main file tree
+export const addFolderToFileTree = (folder) => {
+	return {
+		type: 'ADD_NEW_FOLDER'
+		folder
+	};
+};
+export const removeFolderFileTree = (folder) => {
+	return {
+		type: 'REMOVE_FOLDER';
+		folder
+	};
+};
+
+//Set file tree snippet
+export const setFileTree = (fileTree) => {
+	return {
+		type: 'SET_FILE_TREE'
+		fileTree
+	};
+};
