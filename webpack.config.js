d6e62f2c524ddfe90cfdfb46ca9bb86054beb332
Orlando Castillo <orlandoc@sas.upenn.edu>
2016-03-11 17:16:32 -0800

    implement basic version of webpack, add dist to gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -6,29 +6,20 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
-    './client/index.js'
+    path.join(__dirname, 'client/app/app.js')
   ],
   output: {
     path: path.join(__dirname, 'client/dist'),
     filename: 'bundle.js'
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'ngRedux Async',
-      template: './client/index.html',
-      inject: 'body'
-    }),
-    new webpack.NoErrorsPlugin()
-  ],
   module: {
     loaders: [{
-      test: /\.jsx$/,
-      loaders: ['babel'],
+      test: /\.js$/,
+      loader: 'babel-loader',
       exclude: /node_modules/,
-    },
-    {
-      test: /\.html$/,
-      loader: 'html'
+      query: {
+        presets: ['es2015']
+      }
     }]
   }
 };
