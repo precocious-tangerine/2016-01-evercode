3377bd03ebc297bceb959b2bc24d09b6b5a0f378
Orlando Castillo <orlandoc@sas.upenn.edu>
2016-03-14 11:37:04 -0700

    add Tree Class to fileUtils

---
+++ server/fileUtils.js
@@ -0,0 +1,24 @@
+class Tree {
+  constructor(value) {
+    this.parent = null;
+    this.value = value;
+    this.children = [];
+  }
+  addParent(value) {
+    this.parent = new Tree(value);
+  }
+  addChild(value) {
+    this.children.push(new Tree(value));
+  }
+  deleteChild(value) {
+    this.children = this.children.filter(child => child !== value);
+  }
+  deleteParent() {
+    this.parent = null;
+  }
+}
+
+module.exports = function(snippetObj) {
+
+
+};
