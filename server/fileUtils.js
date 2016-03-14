1d17c40017523d6db2bb0db5d9a1aaeb2b68eda8
Orlando Castillo <orlandoc@sas.upenn.edu>
2016-03-14 12:16:47 -0700

    create helper function to insert into TreeRoot

--- server/fileUtils.js
+++ server/fileUtils.js
@@ -1,3 +1,5 @@
+import R from 'ramda'
+
 class Tree {
   constructor(value) {
     this.parent = null;
@@ -11,16 +13,26 @@ class Tree {
     this.children.push(new Tree(value));
   }
   deleteChild(value) {
-    this.children = this.children.filter(child => child !== value);
+    this.children = this.children.filter(child => !R.equals(child,value));
   }
   deleteParent() {
     this.parent = null;
   }
+  findChild(value) {
+    return R.find(R.equals(value), this.children);
 }
 
 
 let insertIntoTreeRoot = (tree, filePathArrReverse) => {
-  
+  if(filePathArrReverse.length !== 0) {
+    let currValue = filePathArrReverse.pop();
+    let nextValue = filePathArrReverse[fileTree.length -1];
+    this.value ? this.value : currValue;
+    let childNode = tree.findChild(nextValue) || new Tree(null);
+    insertIntoTreeRoot(childNode, filePathArrReverse)
+  }
+}
+
 
 module.exports = function(snippetObj) {
   
