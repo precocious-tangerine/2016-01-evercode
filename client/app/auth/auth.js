9acae17594a21cbd42f45718606f274e26964575
Max Kroshka <max.kroshka@gmail.com>
2016-03-14 15:49:08 -0700

    Move token operations to services

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -10,45 +10,23 @@ export const createAuthCtrl = (url) => {
 }
 
 class AuthCtrl {
-  constructor($location, $window, Auth) {
+  constructor(Auth) {
     this.user = {};
     this.failed = true;
-    this.$location = $location;
-    this.$window = $window;
     this.Auth = Auth;
   }
 
   signin(boolean) {
     this.failed = true;
     if (boolean) {
-      this.Auth.signin(this.user)
-        .then(token => {
-          this.$window.localStorage.setItem('com.evercode', token);
-          this.$location.path('/main');
-        })
-        .catch(error => {
-          this.failed = false;
-          console.error(error);
-        });
+      this.Auth.signin(this.user);
     }
   }
-
 
   signup(boolean) {
     this.failed = true;
     if (boolean) {
-      this.Auth.signup(this.user)
-        .then(token => {
-          console.log(token);
-          console.log(this);
-          this.$window.localStorage.setItem('com.evercode', token);
-          console.log('token is set');
-          this.$location.path('/main');
-        })
-        .catch(error => {
-          this.failed = false;
-          console.error(error);
-        });
+      this.Auth.signup(this.user);
     }
   }
 
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,7 +1,7 @@
 import * as Actions from '../redux/actions.js';
 
 export class Folders {
-  constructor($http, ngRedux) {
+  constructor($http, $ngRedux) {
     this.$http = $http;
     $ngRedux.connect(this.mapStateToThis, this.mapDispatchToThis)(this);
   }
@@ -41,67 +41,93 @@ export class Folders {
 }
 
 export class Snippets {
-  constructor($http) {
+  constructor($http, ngRedux) {
     this.$http = $http;
+    $ngRedux.connect(this.mapStateToThis, this.mapDispatchToThis)(this);
   }
 
-  addSnippet(snippet) {
-    return this.$http({
-      method: 'POST',
-      url: '/snippets',
-      data: snippet
-    });
-  }
+  mapDispatchToThis(dispatch) {
+    return {
+      addSnippet(snippet) {
+        return this.$http({
+          method: 'POST',
+          url: '/snippets',
+          data: snippet
+        });
+      },
 
-  updateSnippet(name, value) {
-    return this.$http({
-      method: 'PUT',
-      url: '/snippets',
-      data: { name, value }
-    });
-  }
+      updateSnippet(name, value) {
+        return this.$http({
+          method: 'PUT',
+          url: '/snippets',
+          data: { name, value }
+        });
+      },
 
-  removeSnippet(snippet) {
-    return this.$http({
-      method: 'POST',
-      url: '/snippets/remove',
-      data: snippet
-    });
+      removeSnippet(snippet) {
+        return this.$http({
+          method: 'POST',
+          url: '/snippets/remove',
+          data: snippet
+        });
+      }
+    }
   }
 }
 
 export class Auth {
-  constructor($http, $location, $window, Folders) {
+  constructor($http, $location, $window, Folders, $ngRedux) {
+    $ngRedux.connect(this.mapStateToThis, this.mapDispatchToThis)(this);
     this.$http = $http;
     this.$location = $location;
     this.$window = $window;
   }
-  signin(user) {
-    return this.$http({
-      method: 'POST',
-      url: '/signin',
-      data: user
-    })
-  }
-  signup(user) {
-    return this.$http({
-      method: 'POST',
-      url: '/signup',
-      data: user
-    })
-  }
+  mapDispatchToThis(dispatch) {
+    return {
+      signin(user) {
+        return this.$http({
+            method: 'POST',
+            url: '/signin',
+            data: user
+          }).then(token => {
+            this.$window.localStorage.setItem('com.evercode', token);
+            this.$location.path('/main');
+          })
+          .catch(error => {
+            this.failed = false;
+            console.error(error);
+          });
+      },
 
-  isAuth() {
-    return !!this.$window.localStorage.getItem('com.evercode');
-  }
+      signup(user) {
+        return this.$http({
+            method: 'POST',
+            url: '/signup',
+            data: user
+          }).then(token => {
+            console.log(token);
+            this.$window.localStorage.setItem('com.evercode', token);
+            this.$location.path('/main');
+          })
+          .catch(error => {
+            this.failed = false;
+            console.error(error);
+          });
+      },
 
-  signout() {
-    return this.$http({
-      method: 'GET',
-      url: '/signout'
-    }).then(function() {
-      this.$window.localStorage.removeItem('com.evercode');
-      this.$location.path('/signin');
-    })
+      isAuth() {
+        return !!this.$window.localStorage.getItem('com.evercode');
+      },
+
+      signout() {
+        return this.$http({
+          method: 'GET',
+          url: '/signout'
+        }).then(function() {
+          this.$window.localStorage.removeItem('com.evercode');
+          this.$location.path('/signin');
+        })
+      }
+    }
   }
 }
