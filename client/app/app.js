63cf4ee9944ddbe67aa45775ece887264218d5b9
Max Kroshka <max.kroshka@gmail.com>
2016-03-14 18:08:14 -0700

    Add model to editor and functions

--- client/app/app.js
+++ client/app/app.js
@@ -4,7 +4,6 @@ import createLogger from 'redux-logger';
 import { finalReducer } from './redux/reducers.js';
 import angular_ui_router from 'angular-ui-router';
 import ui_codemirror from 'angular-ui-codemirror';
-import codemirror from 'codemirror';
 import { createAuthCtrl } from './auth/auth.js';
 import { Folders, Auth, Snippets } from './services/services.js';
 import { snippets } from './snippets/snippets.js';
