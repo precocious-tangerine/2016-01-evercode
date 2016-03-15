b12a3d1102307d2010ce84bf2d292751077e52b5
Max Kroshka <max.kroshka@gmail.com>
2016-03-15 12:12:39 -0700

    Fix a typo

--- server/models/snippets.js
+++ server/models/snippets.js
@@ -2,113 +2,113 @@
 let mongoose = require('mongoose');
 
 let snippetSchema = mongoose.Schema({
-	_createdAt: {type: Date, default: new Date()},
-  	_updatedAt: {type: Date, default: new Date()},
-	createdBy: { type: String, required: true },
-	data: {type: String, required: true},
-	name: {type: String, required: true},
-	filePath: { type: String, required: true},
-	labels: { type: mongoose.Schema.Types.Mixed, default: {} },
-	shareUrl: { type: String },
+  _createdAt: { type: Date, default: new Date() },
+  _updatedAt: { type: Date, default: new Date() },
+  createdBy: { type: String, required: true },
+  data: { type: String, required: true },
+  name: { type: String, required: true },
+  filePath: { type: String, required: true },
+  labels: { type: mongoose.Schema.Types.Mixed, default: {} },
+  shareUrl: { type: String },
 });
 
 let Snippet = mongoose.model('Snippet', snippetSchema);
 
 Snippet.makeSnippet = (snippetObj, callback) => {
-	Snippet.create(snippetObj)
-	  .then((result)=> callback(null, result))
-	  .catch(callback);
+  Snippet.create(snippetObj)
+    .then((result) => callback(null, result))
+    .catch(callback);
 }
 
 Snippet.getSnippet = (_id, callback) => {
-	Snippet.findOne({_id: mongoose.Types.ObjectId(_id)})
-		.then(snippetObj => callback(null, snippetObj))
-		.catch(callback);
+  Snippet.findOne({ _id: mongoose.Types.ObjectId(_id) })
+    .then(snippetObj => callback(null, snippetObj))
+    .catch(callback);
 }
 
 
 Snippet.getSnippetByFilepath = (email, filepath, callback) => {
-	Snippet.findOne({email,filepath})
-		.then(snippetObj => callback(null, snippetObj))
-		.catch(callback);
+  Snippet.findOne({ email, filepath })
+    .then(snippetObj => callback(null, snippetObj))
+    .catch(callback);
 }
 
 Snippet.getSnippetsByUser = (email, callback) => {
-	Snippet.find({createdBy: email})
-		.then((foundSnippets) => {
-			if(Array.isArray(foundSnippets) && foundSnippets.length !== 0) {
-			 	callback(null, foundSnippets);
-			} else {
-			  callback(new Error('no e-mails found'), null);
-			}
-		})
-		.catch(callback);
+  Snippet.find({ createdBy: email })
+    .then((foundSnippets) => {
+      if (Array.isArray(foundSnippets) && foundSnippets.length !== 0) {
+        callback(null, foundSnippets);
+      } else {
+        callback(new Error('no e-mails found'), null);
+      }
+    })
+    .catch(callback);
 }
 
 
 Snippet.getSnippetInfoByUser = (email, callback) => {
-	Snippet.find({createdBy: email}, '-data')
-		.then((foundSnippets) => {
-			if(Array.isArray(foundSnippets) && foundSnippets.length !== 0){
-			 	callback(null, foundSnippets);
-			} else {
-			  callback(new Error('you have no files'), null);
-			}
-		}).catch(callback);
+  Snippet.find({ createdBy: email }, '-data')
+    .then((foundSnippets) => {
+      if (Array.isArray(foundSnippets) && foundSnippets.length !== 0) {
+        callback(null, foundSnippets);
+      } else {
+        callback(new Error('you have no files'), null);
+      }
+    }).catch(callback);
 }
 
 Snippet.getSnippetInfoByFolder = (email, folder, callback) => {
-	Snippet.find({email, filePath: folder + /.+/igm}, '-data')
-		.then((foundSnippets) => {
-			if(Array.isArray(foundSnippets) && foundSnippets.length !== 0){
-			 	callback(null, foundSnippets);
-			} else {
-				callback(new Error('password invalid'), null);
-			}
-		}).catch(callback);
+  Snippet.find({ email, filePath: folder + /.+/igm }, '-data')
+    .then((foundSnippets) => {
+      if (Array.isArray(foundSnippets) && foundSnippets.length !== 0) {
+        callback(null, foundSnippets);
+      } else {
+        callback(new Error('password invalid'), null);
+      }
+    }).catch(callback);
 }
 
 Snippet.getSnippetsByFolder = (email, folder, callback) => {
-	Snippet.find({email, filePath: folder + /.+/igm})
-		.then((foundSnippets) => {
-			if(Array.isArray(foundSnippets) && foundSnippets.length !== 0){
-			 	callback(null, foundSnippets);
-			} else {
-				callback(new Error('password invalid'), null);
-			}
-		}).catch(callback);
+  Snippet.find({ email, filePath: folder + /.+/igm })
+    .then((foundSnippets) => {
+      if (Array.isArray(foundSnippets) && foundSnippets.length !== 0) {
+        callback(null, foundSnippets);
+      } else {
+        callback(new Error('password invalid'), null);
+      }
+    }).catch(callback);
 }
 
 Snippet.updateSnippet = (_id, newProps, callback) => {
   newProps._updatedAt = new Date();
-  Snippet.update({_id: mongoose.Types.ObjectId(_id)}, newProps, {multi: false} , callback);
+  Snippet.update({ _id: mongoose.Types.ObjectId(_id) }, newProps, { multi: false }, callback);
 }
 
 Snippet.makeSubFolder = (email, filepath, callback) => {
-	Snippet.makeSnippet({name:'.config', data:'..', createdBy: email, filePath: filepath + "/"}, callback);
+  Snippet.makeSnippet({ name: '.config', data: '..', createdBy: email, filePath: filepath + "/" }, callback);
 }
 
 Snippet.makeRootFolder = (email, callback) => {
-	Snippet.makeSnippet({name:'.config', data:'..', createdBy: email, filePath: email + "/"}, callback);
+  Snippet.makeSnippet({ name: '.config', data: '..', createdBy: email, filePath: email + "/" }, callback);
 }
 
 Snippet.removeSnippet = (_id, callback) => {
-  Snippet.findOne({_id}).remove(callback);
+  Snippet.findOne({ _id }).remove(callback);
 }
 
 Snippet.removeFolder = (email, folder, callback) => {
-	Snippet.getSnippetsByFolder(email, folder,(err, snippets) => {
-		if(err){
-			callback(err);
-		} else {
-			let allErr = null;
-			snippets.forEach(snippet => {
-				Snippet.removeSnippet(snippet._id, err => allErr = err);
-			}
-			callback(allErr);
-		}
-	})
+  Snippet.getSnippetsByFolder(email, folder, (err, snippets) => {
+    if (err) {
+      callback(err);
+    } else {
+      let allErr = null;
+      snippets.forEach(snippet => {
+        Snippet.removeSnippet(snippet._id, err => allErr = err);
+      });
+      callback(allErr);
+    }
+  })
 }
 
-module.exports = Snippet;
 
+module.exports = Snippet;
