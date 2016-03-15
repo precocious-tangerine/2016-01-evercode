3b7424d17457ee3c686f96cb84360d0359384c24
Orlando Castillo <orlandoc@sas.upenn.edu>
2016-03-15 14:52:45 -0700

    Tie state values and dispatches to snippetCtrl and its template

--- client/app/snippets/snippets.js
+++ client/app/snippets/snippets.js
@@ -1,3 +1,5 @@
+import * as Actions from '../redux/actions.js';
+
 export const snippets = (url) => {
   return {
     url: url,
@@ -11,23 +13,43 @@ export const snippets = (url) => {
 
 class SnippetsCtrl {
   constructor($ngRedux, Snippets) {
-    $ngRedux.connect(this.mapStateToThis)(this);
-    this.data = {};
-    this.data.snippets = [{ name: 'Redux' }, { name: 'Express' }, { name: 'Login' }, { name: 'Auth' }, { name: 'Navbar' }];
-    this.Snippets = Snippets;
+    $ngRedux.connect(this.mapStateToThis, this.mapDispatchToThis)(this);
+    this.folderName = this.selectedFolder.value;
+    
+    this.visibleChildren = [];
+    this.filePath;
+    this.selectedFolder.children.forEach( (child) => {
+      if(typeof child.value === 'string' || child.value.name !== '.config') {
+        visibleChildren.push(Object.assign({},child));
+      } else {
+        var configPath = child.value.name;
+        this.filePath = configPath.substring(0, configPath - 7);
+      }
+    });
+    // this.data = {};
+    // this.data.snippets = [{ name: 'Redux' }, { name: 'Express' }, { name: 'Login' }, { name: 'Auth' }, { name: 'Navbar' }];
+    // this.Snippets = Snippets;
   }
-
   copySnippet(snippet) {
     this.Snippets.getSnippet({ snippetId: snippet._id });
-  };
+  }
 
   favoriteSnippet(snippet) {
     this.Snippets.updateSnippet({ snippetId: snippet._id, value: {favorite: true} });
   }
-
-
   mapStateToThis(state) {
-    const { selectedFolder } = state;
-    return { selectedFolder };
-  };
+    return {
+      selectedFolder: state.selectedFolder
+    };
+  }
+  mapDispatchToThis(dispatch) {
+    return {
+      changeFolder(folderNode) {
+        dispatch(Actions.setSselectedFolder(folderNode));
+      },
+      changeSnippet(snippetObj) {
+        dispatch(Actions.setSelectedSnippet(snippetObj));
+      }
+    };
+  }
 }
