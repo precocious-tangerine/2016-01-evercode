30da235a17735874c31e2d86572a112e65c4a265
Edison Huff <b@mboost.net>
2016-03-15 20:14:44 -0700

    Add outline for all user model functions. currently all broken

--- test/test.js
+++ test/test.js
@@ -38,9 +38,14 @@ describe('the User Model - makeUser', function () {
     email: 'test@chai.com',
     _password: 'just testing'
   };
-
   var tempUser;
   before(function(done) {
+    User.find({email: 'test@chai.com'}, function(err, result) {
+      if (typeof result === 'object') {
+        result.remove();
+      }
+    });
+
     User.makeUser(testUser, function(err, returnedUser) {
       tempUser = returnedUser;
       returnedUser.remove();
@@ -48,7 +53,6 @@ describe('the User Model - makeUser', function () {
     });
   });
   
-
   it('should return an object', function() {
     expect(tempUser).to.be.an('object');
   });
@@ -61,10 +65,129 @@ describe('the User Model - makeUser', function () {
     expect(tempUser).to.have.property('email', 'test@chai.com')
       .that.is.a('string');
   });
-  it('should have a password property that is hashed', function () {
-    expect(tempUser).to.have.property('_password')
+  it('should not have an _password property ', function () {
+    expect(tempUser).to.not.have.property('_password')
       .that.is.a('string')
       .and.not.equal('just testing');
+  });
+});
+
+describe('the User Model - getUser', function () {
+
+  before(function(done) {
+  var testUser = {
+    email: 'test@chai.com',
+    _password: 'just testing'
+  };
+  var tempUser;
+
+  User.find({email: 'test@chai.com'}, function(err, result) {
+    if (typeof result === 'object') {
+      result.remove();
+    }
+  });
+
+    User.makeUser(testUser, function(err, returnedUser) {
+      tempUser = returnedUser;
+      returnedUser.remove();
+      done();
+    });
+
+    User.getUser(testUser._id, function(err, returnedUser) {
+      tempUser = returnedUser;
+      done();
+    });
+  });
+  
+  it('should return an object', function() {
+    expect(tempUser).to.be.an('object');
+  });
+
+  it('should have a unique id called _id', function() {
+    expect(tempUser).to.have.property('_id');
+  });
+  
+  it('should have an email property that is a string', function () {
+    expect(tempUser).to.have.property('email', 'test@chai.com')
+      .that.is.a('string');
+  });
+  it('should have a password property', function () {
+    expect(tempUser).to.not.have.property('_password')
+  });
+});
+
+describe('the User Model - updateUser', function () {
+
+  var updateResult;
+  var userUpdates = {
+    bio: 'I am a test',
+  }
+
+  before(function(done) {
+    User.updateUser(testUser._id, userUpdates ,function(err, result) {
+      updateResult = result;
+        User.find({_id: testUser._id}, function(err, returnedUser) {
+          testUser = returnedUser;
+          returneduser.remove();
+          done();
+        })
+    });
+  });
+  
+  it('should return a results object', function() {
+    expect(updateResult).to.be.an('object');
+  });
+
+  it('should update the document in the database', function() {
+    expect(updateResult).to.have.property('n')
+      .that.equals(1);
+  });
+  
+  it('should have an updated bio property that is a string', function () {
+    expect(tempUser).to.have.property('bio', 'I am a test')
+      .that.is.a('string');
+  });
+});
+
+describe('the User Model - removeUser', function () {
+  before(function(done) {
+    var testUser = {
+      email: 'test@chai.com',
+      _password: 'just testing'
+    };
+    var deleteResult;
+
+    User.create(testUser)
+      .then(function(returnedUser){
+        testUser = returnedUser;
+        User.removeUser(testUser._id ,function(err, result) {
+          deleteResult = result;
+          done();
+        })
+      })
+      .catch(function(err){
+        console.log(err);
+        done()
+      }) 
+  });
+  
+  it('should return a results object', function() {
+    expect(deleteResult).to.be.an('object');
+  });
+
+  it('should report removal from the database', function() {
+    expect(updateResult).to.have.property('n')
+      .that.equals(1);
+  });
+  
+  it('should not be able to find the test user', function (done) {
+    User.find({_id: testUser._id}, function(err, result){
+      expect(result).to.be.null;
+      done();
+      if (result) {
+        result.remove();
+      }
+    });
   });
 });
 
