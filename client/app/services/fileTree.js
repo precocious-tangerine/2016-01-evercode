763beb5797a8b086cd88469adf372bce3be8974a
Orlando Castillo <orlandoc@sas.upenn.edu>
2016-03-17 22:30:21 -0700

    Add helper methods to TreeMap Class

--- client/app/services/fileTree.js
+++ client/app/services/fileTree.js
@@ -9,36 +9,61 @@ class TreeMap {
       } else {
         let currPath = prevPath + '/' + currItem;
         let parentObj = context[prevPath], currObj = context[currPath];
-
         if(prevPath && !parentObj) { 
           context[prevPath] = {filePath: prevPath, children: []}
         }
         if(parentObj && parentObj.children.indexOf(currPath) === -1) {
           context[prevPath].children.push(currPath);
         }
-
         context[currPath] = currObj ? currObj : {children: []};
         context[currPath].parent = prevPath;
         context[currPath].value = currItem;
         context[currPath].filePath = currPath;
-
         return currPath;
       }         
     }, "");
   }
+  getParent(filePath) {
+    let parentPath = this[filePath].parent
+    return this[parentPath];
+  }
+  getChildren(filePath) {
+    let children = this[filePath].children;
+    return children.map(childPath => {this[childPath]});
+  }
+  getAllParents(filePath) {
+    let results = [], parent;
+    while (parent = this.getParent(filePath)) {
+      results.push(parent);
+    }
+    return parent;
+  }
+  getAllChildren(filePath) {
+    let children = this.getChildren(filePath);
+    if(children.length === 0) {
+      return [];
+    } else {
+      return children.reduce((results, child) => {
+        let childPath = child.filePath;
+        return results.concat(child, this.getChildren[childPath]);  
+      }.bind(this), []);
+    }
+  }
+  deleteNode(filePath) {
+    let childrenPaths = this.getAllChildren(filePath).map(child => child.filePath);
+    let parent = this.getParent(filePath);
+    childrenPaths.forEach(childPath => {
+      delete this[childPath];
+    });
+    parent.children = parent.children.filter(childPath => childPath !== filePath);
+  }
 }
 
-
-
 let convertToTree = function(snippetObj) {
- 
   let userTreeMap = new TreeMap();
-
-
   Object.keys(snippetObj).forEach((key) => {
     userTreeMap.insertNode(key, snippetObj[key]);
   })
-  window.test = userTreeMap;
   return userTreeMap
 }
 
