a8e56315174a4a4dc832cd4c7767c36078e44ecf
Orlando Castillo <orlandoc@sas.upenn.edu>
2016-03-17 21:19:28 -0700

    Refactor fileTree with different implementation

--- client/app/services/fileTree.js
+++ client/app/services/fileTree.js
@@ -47,27 +47,55 @@ let insertIntoTreeRoot = (tree, filePathArrReverse, filePathAttr, callback) => {
 let convertToTree = function(snippetObj) {
   let keyValues = R.toPairs(snippetObj);
   let filePaths = keyValues.map((keyValue) => {
-    let folders = keyValue[0].split('/');
-    folders[0] ? null : folders.shift();
-    folders[folders.length - 1] ? null : folders.pop();
+    let folders = keyValue[0].split('/').filter(a => a);
     folders[folders.length - 1] = keyValue[1];
-    return R.reverse(folders);
+    return folders;
   });
-  let userTree = new Tree(null);
-  let userTreeMapCopy = {};
-  filePaths.forEach((filePath) => {
-    insertIntoTreeRoot(userTree, filePath, '', node => {
-      userTreeMapCopy[node.filePath] = node;
-    });
-  })
-  userTreeMapCopy.__root = userTree;
+
   let userTreeMap = {};
-  Object.keys(userTreeMapCopy).forEach(key => {
-    Object.assign(userTreeMap[key] = {}, { value: userTreeMapCopy[key].value, filePath: userTreeMapCopy[key].filePath });
-    userTreeMap[key].children = userTreeMapCopy[key].children.map(tree => tree.filePath);
-    userTreeMap[key].parent = userTreeMapCopy[key].parent ? userTreeMapCopy[key].parent.filePath : null;
-  })
-  return userTreeMap;
+  Object.defineProperty(userTreeMap, 'insertNode', {
+    value: function(filePath, node) {
+      let folders = filePath.split('/').filter(a => a);
+      folders.reduce((prevPath, currItem) => {
+        let currPath = prevPath + '/' + currItem;
+        let parentObj = this[prevPath], currObj = this[currPath];
+        
+        parentObj ? parentObj : {};
+        parentObj.children ? parentObj.children.push(currPath) : [currPath];
+        parentObj.filePath = prevPath;
+
+        currObj ? currObj : {};
+        currObj.parent = parentPath;
+        currObj.children = [];
+        currObj.value = currItem;
+        currObj.filePath = currPath;
+        return currPath;         
+      });
+    }
+  });
+
+  filePaths.forEach((filePath) => userTreeMap.insertNode(filePath));
+  return userTreeMap
+}
+
+
+
+
+  // let userTree = new Tree(null);
+  // let userTreeMapCopy = {};
+  // filePaths.forEach((filePath) => {
+  //   insertIntoTreeRoot(userTree, filePath, '', node => {
+  //     userTreeMapCopy[node.filePath] = node;
+  //   });
+  // })
+  // userTreeMapCopy.__root = userTree;
+  // let userTreeMap = {};
+  // Object.keys(userTreeMapCopy).forEach(key => {
+  //   Object.assign(userTreeMap[key] = {}, { value: userTreeMapCopy[key].value, filePath: userTreeMapCopy[key].filePath });
+  //   userTreeMap[key].children = userTreeMapCopy[key].children.map(tree => tree.filePath);
+  //   userTreeMap[key].parent = userTreeMapCopy[key].parent ? userTreeMapCopy[key].parent.filePath : null;
+  // })
+  // return userTreeMap;
 };
 
 export default convertToTree;
