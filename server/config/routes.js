a20f6b36f85479589639d8f6e2f4b09b66272919
Max Kroshka <max.kroshka@gmail.com>
2016-03-18 20:09:04 -0700

    Redirect to main after login

--- server/config/routes.js
+++ server/config/routes.js
@@ -5,10 +5,9 @@ var qs = require('querystring');
 var config = require('../config');
 var Users = Promise.promisifyAll(require('../models/users'));
 var Snippets = Promise.promisifyAll(require('../models/snippets'));
-var Annotations = Promise.promisifyAll(require('../models/annotations'))
 var jwt = require('jsonwebtoken');
 var secret = config.secretToken;
-let {postSignup, getVerification} = require('./email-verification.js');
+
 let createJWT = (user) => {
   var payload = {
     sub: user._id,
@@ -17,18 +16,8 @@ let createJWT = (user) => {
   return jwt.sign(payload, secret);
 }
 
+
 module.exports = (app, express) => {
-  app.route('/send-email')
-    .post(postSignup)
-
-  app.route('/email-verification/:URL')
-    .get(getVerification)
-
-  app.route('/orlandoc01/test/snippet.txt')
-    .get((req, res) => {
-      res.send({data: 'test1'});
-    });
-
   app.route('/signin')
     .post((req, res) => {
       let { email, password } = req.body;
@@ -36,9 +25,8 @@ module.exports = (app, express) => {
       //Do some comparing
       Users.checkCredentialsAsync(email, password)
         .then((userData) => {
-          let user = { name: userData.name, avatar_url: userData.avatar_url };
           token = createJWT({ email });
-          res.status(201).send({ token: token, user: user });
+          res.status(201).send({ token });
         }).catch((err) => {
           console.log(err);
           res.status(401).send('Unauthorized');
@@ -50,9 +38,23 @@ module.exports = (app, express) => {
       let { email, password } = req.body;
       let token;
       Users.makeUserAsync({ email, _password: password })
-        .then((userData) => {
+        .then(userData => {
           token = createJWT({ email });
           res.status(201).send(token);
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).send(err);
+        });
+    });
+
+  app.route('/api/userInfo')
+    .get((req, res) => {
+      let email = jwt.verify(req.headers.authorization, secret).email;
+      Users.getUserAsync(email)
+        .then(userData => {
+          let user = { name: userData.name, avatar_url: userData.avatar_url, email: email };
+          res.status(201).send(user);
         })
         .catch((err) => {
           console.log(err);
@@ -63,7 +65,7 @@ module.exports = (app, express) => {
   app.route('/api/snippets')
     .get((req, res) => {
       Snippets.getSnippetAsync(req.query["_id"])
-        .then((snippet) => {
+        .then(snippet => {
           if (snippet) {
             res.status(200).send(snippet)
           } else {
@@ -78,7 +80,7 @@ module.exports = (app, express) => {
       let email = jwt.verify(req.headers.authorization, secret).email;
       req.body.createdBy = email;
       Snippets.makeSnippetAsync(req.body)
-        .then((snippet) => {
+        .then(snippet => {
           res.status(201).send(snippet)
         }).catch((err) => {
           console.log(err);
@@ -87,7 +89,7 @@ module.exports = (app, express) => {
     })
     .delete((req, res) => {
       Snippets.removeSnippetAsync(req.query.snippetId)
-        .then((response) => {
+        .then(response => {
           if (response) {
             res.status(201).send(response);
           } else {
@@ -100,7 +102,7 @@ module.exports = (app, express) => {
     })
     .put((req, res) => {
       Snippets.updateSnippetAsync(req.body.snippetId, req.body.value)
-        .then((success) => {
+        .then(success => {
           if (success) {
             Snippets.getSnippetAsync(req.body.snippetId).then((snippet) => {
               if (snippet) {
@@ -122,7 +124,7 @@ module.exports = (app, express) => {
     .get((req, res) => {
       let token = jwt.verify(req.headers.authorization, secret);
       return Snippets.getSnippetsByUserAsync(token.email)
-        .then((results) => {
+        .then(results => {
           if (Array.isArray(results) && results.length > 0) {
             var fileTreeObj = {};
             results.forEach((node) => {
@@ -137,33 +139,13 @@ module.exports = (app, express) => {
           res.status(500).send(err);
         })
     });
-    
-  app.route('/api/snippet/annotations')
-    .get((req, res) => {
-      let {_sid} = req.query["_id"];
-      return Annotations.getBySnippetAsync(_sid)
-        .then((results) => {
-          if (Array.isArray(results) && results.length > 0) {
-            var idTreeObj = {};
-            results.forEach((node) => {
-              idTreeObj[node._id] = node;
-            });
-            res.status(200).send(idTreeObj);
-          } else {
-            res.status(404).send("Annotations not Found");
-          }
-        }).catch((err) => {
-          console.log(err);
-          res.status(500).send(err);
-        })
-    });
 
   app.route('/api/folders/')
     .post((req, res) => {
       let email = jwt.verify(req.headers.authorization, secret).email;
       let path = req.body.path;
       Snippets.makeSubFolderAsync(email, path)
-        .then((folder) => {
+        .then(folder => {
           res.status(201).send(folder)
         }).catch((err) => {
           console.log(err);
@@ -174,7 +156,7 @@ module.exports = (app, express) => {
       let email = jwt.verify(req.headers.authorization, secret).email;
       let path = req.query.filePath;
       Snippets.removeFolderAsync(email, path)
-        .then((result) => {
+        .then(result => {
           if (result) {
             res.status(201).send("Succesfully removed");
           } else {
@@ -185,64 +167,6 @@ module.exports = (app, express) => {
           res.status(500).send(err);
         })
     })
-
-  app.route('/api/annotations/')
-    .get((req, res) => {
-      Annotations.getAnnotationAsync(req.query["_id"])
-        .then((annotation) => {
-          if (annotation) {
-            res.status(200).send(annotation)
-          } else {
-            res.status(404).send("Annotation not Found");
-          }
-        }).catch((err) => {
-          console.log(err);
-          res.status(500).send(err);
-        })
-    })
-    .post((req, res) => {
-      let email = jwt.verify(req.headers.authorization, secret).email;
-      req.body._createdBy = email;
-      Annotations.makeAnnotationAsync(req.body)
-        .then((annotation) => {
-          res.status(201).send(annotation)
-        }).catch((err) => {
-          console.log(err);
-          res.status(500).send(err);
-        })
-    })
-    .delete((req, res) => {
-      Annotations.removeAnnotationAsync(req.query.snippetId)
-        .then((response) => {
-          if (response) {
-            res.status(201).send(response);
-          } else {
-            res.status(404).send("Annotation not Found");
-          }
-        }).catch((err) => {
-          console.log(err);
-          res.status(500).send(err);
-        })
-    })
-    .put((req, res) => {
-      Annotations.updateAnnotationAsync(req.body.snippetId, req.body.value)
-        .then((success) => {
-          if (success) {
-            Annotations.getAnnotationAsync(req.body.annotationId).then((annotation) => {
-              if (annotation) {
-                res.status(201).send(annotation);
-              } else {
-                res.status(404).send("Annotation not Found");
-              }
-            })
-          } else {
-            res.status(404).send("Annotation not Found");
-          }
-        }).catch((err) => {
-          console.log(err);
-          res.status(500).send(err);
-        })
-    });
 
   app.post('/auth/github', (req, res) => {
     var accessTokenUrl = 'https://github.com/login/oauth/access_token';
@@ -265,16 +189,14 @@ module.exports = (app, express) => {
           if (existingUser) {
             Users.updateUserAsync(profile.email, { github: profile.id, avatar_url: profile.avatar_url, name: profile.name }).then((success) => {
               var token = createJWT({ email: existingUser.email });
-              let user = { name: existingUser.name, avatar_url: existingUser.avatar_url };
-              res.status(201).send({ token: token, user: user });
+              res.status(201).send({ token });
             })
           } else {
             let { email, github, avatar_url, name, id } = profile;
             Users.makeUserAsync({ email, github, avatar_url, name, github: '' + id })
               .then((userObj) => {
                 token = createJWT({ email: userObj.email });
-                let user = { name: userObj.name, avatar_url: userObj.avatar_url };
-                res.status(201).send({ token: token, user: user });
+                res.status(201).send({ token });
               })
               .catch((err) => {
                 console.log(err);
