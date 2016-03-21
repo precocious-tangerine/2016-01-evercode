a329318bebc85639c16242144400aa1182306eab
Orlando Castillo <orlandoc@sas.upenn.edu>
2016-03-21 14:39:21 -0700

    Refactor code in get_token and get_url

--- server/config/gmail_config/get_url.js
+++ server/config/gmail_config/get_url.js
@@ -2,32 +2,32 @@ var fs = require('fs');
 var googleAuth = require('google-auth-library');
 
 
+const getAuthorizationUrl = (cb) => {
+  // Load client secrets
+  fs.readFile(__dirname + '/../../client_secret.json', function(err, data) {
+    if (err) {
+      cb(err);
+    } else {
+      let credentials = JSON.parse(data);
+      let clientSecret = credentials.installed.client_secret;
+      let clientId = credentials.installed.client_id;
+      let redirectUrl = credentials.installed.redirect_uris[0];
+      let auth = new googleAuth();
+      let oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
 
-  function getAuthorizationUrl(cb) {
-    // Load client secrets
-    fs.readFile(__dirname + '/../../client_secret.json', function(err, data) {
-      if (err) {
-        return cb(err);
-      }
-      var credentials = JSON.parse(data);
-      var clientSecret = credentials.installed.client_secret;
-      var clientId = credentials.installed.client_id;
-      var redirectUrl = credentials.installed.redirect_uris[0];
-      var auth = new googleAuth();
-      var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
-
-      var authUrl = oauth2Client.generateAuthUrl({
+      let authUrl = oauth2Client.generateAuthUrl({
         access_type: 'offline',
         scope: ['https://www.googleapis.com/auth/gmail.send']
       });
-      return cb(null, authUrl);
-    });
-  }
-
-  getAuthorizationUrl(function(err, url) {
-    if (err) {
-      console.log('err:', err);
-    } else {
-      console.log('Authorization url is:\n', url);
+      cb(null, authUrl);
     }
   });
+}
+
+getAuthorizationUrl(function(err, url) {
+  if (err) {
+    console.log('err:', err);
+  } else {
+    console.log('Authorization url is:\n', url);
+  }
+});
