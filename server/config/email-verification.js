abf2b5b9265fd3a97f214b58eacb3e37f489a8c9
Orlando Castillo <orlandoc@sas.upenn.edu>
2016-03-21 15:39:37 -0700

    Fix email signup and verification and now stores hash instead of pw

--- server/config/email-verification.js
+++ server/config/email-verification.js
@@ -6,18 +6,7 @@ let bcrypt = Promise.promisifyAll(require('bcrypt'));
 let nev = Promise.promisifyAll(require('email-verification')(mongoose));
 let User = require('../models/users.js');
 
-console.log('testing email verification setup');
 
-
-let myHashingFunction = (password, tempUserData, inserTempUser, callback) => {
-  bcrypt.genSaltAsync(13)
-  .then((salt) => bcrypt.hashAsync(password, salt))
-  .then((hash) => {
-    return insertTempUser(hash, tempUserData, callback);
-  });
-}
-
-console.log(typeof nev.configureAsync);
 nev.configureAsync({
   persistentUserModel: User,
   expirationTime: 600,
@@ -29,7 +18,6 @@ nev.configureAsync({
         pass: 'donkeydonkey'
       } 
   },
-  hashingFunction: myHashingFunction,
   passwordFieldName: '_password',
 })
 
@@ -41,29 +29,24 @@ nev.generateTempUserModel(User, (err, tempUser) => {
   }
 });
 
-//.then((options) => {
-//  console.log('configured: ', options)
-//  return nev.generateTempUserModelAsync(User);
-//}).then((tempUserModel) => console.log('generated temp user', tempUserModel))
-//.catch(err => {
-//  console.log('err in config is', err);
-//});
 
-console.log('finished verification');
 
 module.exports.postSignup = (req, res) => {
    let { email, password } = req.body;
-   let newUser = new User({email, _password: password});
-   nev.createTempUserAsync(newUser)
+   bcrypt.genSaltAsync(13)
+   .then(salt => bcrypt.hashAsync(password, salt))
+   .then(hash => {
+     console.log('hash is', hash);
+     let newUser = new User({email, _password: hash});
+     return nev.createTempUserAsync(newUser)
+   })   
    .then((newTempUser) => {
-     console.log('inside first then block');
+     console.log('user confirmed:, ', newTempUser);
       if(newTempUser) {
-        console.log('newTempUser is', newTempUser);
-        newTempUser.save();
         let URL = newTempUser[nev.options.URLFieldName];
         return nev.sendVerificationEmailAsync(email, URL)
          .then((info) => {
-          res.send('an email has been sent to you. Please verify it\n, info:', info);
+          res.send('an email has been sent to you. Please verify it\n, info: ' + JSON.stringify(info));
          }).catch(err => {
            console.log('err is ', err);
            res.status(404).send('failed to send');
@@ -81,15 +64,20 @@ module.exports.getVerification = (req, res) => {
   nev.confirmTempUserAsync(url)
   .then((user) => {
     if(user) {
-      nev.sendVerificationEmailAsync(user.email)
+      nev.sendConfirmationEmailAsync(user.email)
       .then((info) => {
-        res.send('You have been confirmed');
-        res.send(info);
+        res.send('You have been confirmed, info ' + JSON.stringify(info));
       })
-      .catch(err => res.status(404).send(err));
+      .catch(err => {
+        console.log('err: sending verify email', err);
+        res.status(404).send(err)
+      });
     } else {
       res.status(404).send('Error: confirming temp user FAILED');
     }
   })
-  .catch(err => res.status(404).send('failed'));
+  .catch(err => {
+    console.log('err is', err); 
+    res.status(404).send('failed');
+  });
 }
