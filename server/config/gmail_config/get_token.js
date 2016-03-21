549c15ae810bab9219710b59d8a4293c53d1437b
Orlando Castillo <orlandoc@sas.upenn.edu>
2016-03-21 14:57:20 -0700

    Re-implement gmailConfig with promises

--- server/config/gmail_config/get_token.js
+++ server/config/gmail_config/get_token.js
@@ -1,28 +1,26 @@
-var fs = require('fs');
-var googleAuth = require('google-auth-library');
+'use strict';
+var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require('fs'));
+var googleAuth = Promise.promisifyAll(require('google-auth-library'));
 
-const getAuthorizationToken = (code, cb) => {
+const getAuthorizationToken = (code) => {
   // Load client secrets
-  fs.readFile(__dirname + '/../../client_secret.json', (err, data) => {
-    if (err) {
-      cb(err);
-    } else {
-      let credentials = JSON.parse(data);
-      let clientSecret = credentials.installed.client_secret;
-      let clientId = credentials.installed.client_id;
-      let redirectUrl = credentials.installed.redirect_uris[0];
-      let auth = new googleAuth();
-      let oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+  return fs.readFileAsync(__dirname + '/../../client_secret.json')
+  .then(data => {
+    let credentials = JSON.parse(data);
+    let clientSecret = credentials.installed.client_secret;
+    let clientId = credentials.installed.client_id;
+    let redirectUrl = credentials.installed.redirect_uris[0];
+    let auth = new googleAuth();
+    let oauth2Client = Promise.promisifyAll(new auth.OAuth2(clientId, clientSecret, redirectUrl));
 
-      oauth2Client.getToken(code, (err, token) => {
-        if (err) {
-          cb(err);
-        } else {
-          let file = 'gmail-credentials.json';
-          fs.writeFile(file, JSON.stringify(token));
-          cb(null, file);
-        }
-      });
+    return oauth2Client.getTokenAsync(code)
+    .then(token => {
+      let file = 'gmail-credentials.json';
+      return fs.writeFile(file, JSON.stringify(token))
+               .then(() => file)
+    })
+    .catch(err => console.log('err is ', err));
   });
 }
 
@@ -30,12 +28,9 @@ if (process.argv.length != 3) {
   console.log('usage: node get_token token');
   process.exit(1);
 }
+
 let token = process.argv[2];
 
-getAuthorizationToken(token, (err, file) => {
-  if (err) {
-    console.log('err:', err);
-  } else {
-    console.log('authorization token is in:\n', file);
-  }
-});
+getAuthorizationToken(token)
+.then(file => console.log('authorization token is in:\n', file))
+.catch(err => console.log('Err: ', err));
--- server/config/gmail_config/get_url.js
+++ server/config/gmail_config/get_url.js
@@ -1,33 +1,29 @@
-var fs = require('fs');
-var googleAuth = require('google-auth-library');
+'use strict';
+var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require('fs'));
+var googleAuth = Promise.promisifyAll(require('google-auth-library'));
 
 
 const getAuthorizationUrl = (cb) => {
   // Load client secrets
-  fs.readFile(__dirname + '/../../client_secret.json', function(err, data) {
-    if (err) {
-      cb(err);
-    } else {
-      let credentials = JSON.parse(data);
-      let clientSecret = credentials.installed.client_secret;
-      let clientId = credentials.installed.client_id;
-      let redirectUrl = credentials.installed.redirect_uris[0];
-      let auth = new googleAuth();
-      let oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+  return fs.readFileAsync(__dirname + '/../../client_secret.json')
+  .then( (data) => {
+    let credentials = JSON.parse(data);
+    let clientSecret = credentials.installed.client_secret;
+    let clientId = credentials.installed.client_id;
+    let redirectUrl = credentials.installed.redirect_uris[0];
+    let auth = new googleAuth();
+    let oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
 
-      let authUrl = oauth2Client.generateAuthUrl({
-        access_type: 'offline',
-        scope: ['https://www.googleapis.com/auth/gmail.send']
-      });
-      cb(null, authUrl);
-    }
-  });
+    let authUrl = oauth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: ['https://www.googleapis.com/auth/gmail.send']
+    })
+    return authUrl
+  })
 }
 
-getAuthorizationUrl(function(err, url) {
-  if (err) {
-    console.log('err:', err);
-  } else {
-    console.log('Authorization url is:\n', url);
-  }
-});
+getAuthorizationUrl()
+.then((url) => console.log('Authorization url is:\n', url))
+.catch(err => console.log('err is', err));
+
