b68698ceb99455776c512afce27482ac7f7b1cbb
Edison Huff <b@mboost.net>
2016-03-23 15:49:24 -0700

    Fixes Snippet.getPublic. No longer returns folders

--- server/models/snippets.js
+++ server/models/snippets.js
@@ -117,12 +117,12 @@ Snippet.removeFolder = (email, folder, callback) => {
   })
 }
 
-Snippet.getPublicSnippets = (callback) => {
-  Snippet.find({public: true})
-    .sort({ createdAt: -1 } )
+//TODO figure out how to return only 25
+Snippet.getPublic = (callback) => {
+  Snippet.find({public: 1, name: {$ne: '.config'}})
     .then((foundSnippets) => {
       if (Array.isArray(foundSnippets) && foundSnippets.length !== 0) {
-        callback(null, foundSnippets);
+        callback(null, foundSnippets.sort({ _createdAt: -1 }));
       } else {
         callback(new Error('Error'), null);
       }
